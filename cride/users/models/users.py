f29182771c73b06ee86e8969fb3945389135062c
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-05-10 18:48:48 -0500

    organizing models in a Django package

--- cride/users/models/users.py
+++ cride/users/models/users.py
@@ -3,6 +3,7 @@
 # Django 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 
 # Utilities
 from cride.utils.models import CRideModel
@@ -18,17 +19,21 @@ class User(CRideModel, AbstractUser):
     email = models.EmailField(
         'email address',
         unique=True,
-        error_message={
+        error_messages={
             'unique': 'A user with that email already exists'
         }
     )
 
-    phone_number = models.CharField(max_length=17, blank=True)
+    phone_regex = RegexValidator(
+        regex=r'\+?1?\d{9,15}$',
+        message="Phone number must be entered in the format: +999999999. Up to 15 digits allowed."
+    )
+    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = ['username', 'first_name', 'last_name']
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
-    is_client = models.BooleandField(
+    is_client = models.BooleanField(
         'client status',
         default=True,
         help_text=(
@@ -37,8 +42,16 @@ class User(CRideModel, AbstractUser):
         )
     )
 
-    is_verified = models.BooleandField(
+    is_verified = models.BooleanField(
         'verified',
         default=True,
         help_text='Set to true when the user have verified its email address'
     )
+
+    def __str__(self):
+        """Return username."""
+        return self.username
+
+    def get_short_name(self):
+        """Return username."""
+        return self.username
