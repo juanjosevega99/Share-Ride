6cdd2eb92e5d9c2cff629e621dde279dce3d6491
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-05-11 11:04:55 -0500

    solving challenge

--- cride/users/models/profiles.py
+++ cride/users/models/profiles.py
@@ -1,23 +1,24 @@
 """Profile model."""
 
-# Django 
+# Django
 from django.db import models
 
 # Utilities
 from cride.utils.models import CRideModel
 
+
 class Profile(CRideModel):
     """Profile model.
-    
+
     A profile holds a user's public data like biography, picture,
-    and stattistics
+    and statistics.
     """
 
-    users = models.OneToOneField('users.User', on_delete=models.CASCADE)
+    user = models.OneToOneField('users.User', on_delete=models.CASCADE)
 
     picture = models.ImageField(
         'profile picture',
-        upload_to='users/pictures&/',
+        upload_to='users/pictures/',
         blank=True,
         null=True
     )
@@ -26,9 +27,9 @@ class Profile(CRideModel):
     # Stats
     rides_taken = models.PositiveIntegerField(default=0)
     rides_offered = models.PositiveIntegerField(default=0)
-    rides_taken = models.FloatField(
-        default=5.0, 
-        help_text="User's reputation based on the rides taken and offered"
+    reputation = models.FloatField(
+        default=5.0,
+        help_text="User's reputation based on the rides taken and offered."
     )
 
     def __str__(self):
